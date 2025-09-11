ecc8240262f7eb513535090bfc272779d6684d85
Zharkov Aleksey <gross3000@yandex.ru>
2025-09-11 19:51:07 +0300

    correction tests/test_reading_json

--- tests/test_reading_files.py
+++ tests/test_reading_files.py
@@ -7,4 +7,6 @@ from src.reading_files import reading_json
 @patch("json.load")
 def test_reading_json(mock_get, list_category):
     mock_get.return_value = list_category
-    assert reading_json(os.path.abspath("../data/products.json"))[0].name == list_category[0].get("name")
+    current_dir = os.path.dirname(__file__)
+    file = os.path.join(current_dir, "..", "data", "products.json")
+    assert reading_json(file)[0].name == list_category[0].get("name")
